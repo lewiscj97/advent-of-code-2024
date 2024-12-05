8fd65e02aec3614b845caabec53d7388b85317ad
lewis.jones <lewis1997@live.com>
2024-12-05 21:54:26 +0000

    refactor: tests are simplified

--- day-2/test.js
+++ day-2/test.js
@@ -3,21 +3,17 @@ const expect = require('chai').expect;
 const { validateReport } = require('./day2');
 
 describe('Day 2', () => {
-  it('should return safe when all input vals ascend by 1', () => {
-    const testInput = "1 2 3 4 5";
+  describe('Ascending values', () => {
+    const ascendingTests = [
+      "1 2 3 4 5",
+      "2 4 6 8 10 12",
+      "7 10 13 16 19 22 25"
+    ];
 
-    expect(validateReport(testInput)).to.be.true;
-  });
-
-  it('should return safe when all input vals ascend by 2', () => {
-    const testInput = "2 4 6 8 10 12";
-
-    expect(validateReport(testInput)).to.be.true;
-  });
-
-  it('should return safe when all input vals ascend by 3', () => {
-    const testInput = "7 10 13 16 19 22 25";
-
-    expect(validateReport(testInput)).to.be.true;
+    ascendingTests.forEach((val) => {
+      it('should return true', () => {
+        expect(validateReport(val)).to.be.true;
+      });
+    });
   });
 });
