79e8da77e17cad65619676ebec5f868660ccfe64
lewis.jones <lewis1997@live.com>
2024-12-06 19:15:42 +0000

    feat: count xmas in a line

--- day-4/day4.js
+++ day-4/day4.js
@@ -1,3 +1,24 @@
+function countXmas(matrix) {
+  const count = _inLine(matrix);
+  return count;
+}
+
+function _inLine(matrix) {
+  let count = 0;
+  matrix.forEach((row) => {
+    for (let i = 0; i < row.length; i++) {
+      const val = row[i];
+      if (val === 'X') {
+        if ('X' + row[i+1] + row[i+2] + row[i+3] === 'XMAS') {
+          count += 1;
+        }
+      }
+    }
+  });
+
+  return count;
+}
+
 function createMatrix(input) {
   const split = input.split('\n');
   const matrix = [];
@@ -9,4 +30,4 @@ function createMatrix(input) {
   return matrix;
 }
 
-module.exports = { createMatrix }
+module.exports = { createMatrix, countXmas }
--- day-4/test.js
+++ day-4/test.js
@@ -1,30 +1,50 @@
 const expect = require('chai').expect;
-const { createMatrix } = require("./day4");
+const { createMatrix, countXmas } = require("./day4");
 
 describe('Day 4', () => {
-  it('should break single line into single line matrix', () => {
-    const input = "XMAS";
-    const expected = [['X', 'M', 'A', 'S']];
+  describe('Matrices creation', () => {
+    it('should break single line into single line matrix', () => {
+      const input = "XMAS";
+      const expected = [['X', 'M', 'A', 'S']];
 
-    const output = createMatrix(input);
+      const output = createMatrix(input);
 
-    expect(output).to.deep.eq(expected);
+      expect(output).to.deep.eq(expected);
+    });
+
+    it('should break multiple lines into matrix', () => {
+      const input =
+        "MMMSXXMASM\n" +
+        "MSAMXMSMSA\n" +
+        "AMXSXMAAMM\n" +
+        "MSAMASMSMX\n" +
+        "XMASAMXAMM\n" +
+        "XXAMMXXAMA\n" +
+        "SMSMSASXSS\n" +
+        "SAXAMASAAA\n" +
+        "MAMMMXMMMM\n" +
+        "MXMXAXMASX";
+
+      const expected = [
+        ['M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M'],
+        ['M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'],
+        ['A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'],
+        ['M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X'],
+        ['X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M'],
+        ['X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A'],
+        ['S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S'],
+        ['S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A'],
+        ['M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'],
+        ['M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X']
+      ]
+      const output = createMatrix(input);
+
+      expect(output).to.deep.eq(expected);
+    });
   });
 
-  it('should break multiple lines into matrix', () => {
-    const input =
-      "MMMSXXMASM\n" +
-      "MSAMXMSMSA\n" +
-      "AMXSXMAAMM\n" +
-      "MSAMASMSMX\n" +
-      "XMASAMXAMM\n" +
-      "XXAMMXXAMA\n" +
-      "SMSMSASXSS\n" +
-      "SAXAMASAAA\n" +
-      "MAMMMXMMMM\n" +
-      "MXMXAXMASX";
-
-    const expected = [
+  describe('Counting xmas', () => {
+    const matrix = [
       ['M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M'],
       ['M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'],
       ['A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'],
@@ -36,8 +56,12 @@ describe('Day 4', () => {
       ['M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'],
       ['M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X']
     ]
-    const output = createMatrix(input);
 
-    expect(output).to.deep.eq(expected);
-  });
+    it('should identify xmas in a line', () => {
+      const expected = 3;
+      const output = countXmas(matrix);
+
+      expect(output).to.eq(expected);
+    });
+  })
 });
