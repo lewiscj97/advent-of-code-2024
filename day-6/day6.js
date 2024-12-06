777e382d9c4db16125e99ed3c2d39357879c7b37
lewis.jones <lewis1997@live.com>
2024-12-06 22:42:49 +0000

    refactor: clean up solution

--- day-6/day6.js
+++ day-6/day6.js
@@ -1,31 +1,22 @@
-// Need to find the location of the guard in the matrix
-// need to identify direction of travel
 // check what is in the cell directly in the direction of the guard
 // move if they can
+
+const DIRECTIONS = {
+  '^': 'UP',
+  '>': 'RIGHT',
+  '<': 'LEFT',
+  'v': 'DOWN',
+};
 
 function locateGuard(matrix) {
   let location = {}
   matrix.forEach((row, x) => {
     row.forEach((value, y) => {
       if (value !== '.' && value !== '#') {
-        let direction;
-        if (value === '^') {
-          direction = 'UP';
-        }
-        if (value === '>') {
-          direction = 'RIGHT';
-        }
-        if (value === '<') {
-          direction = 'LEFT';
-        }
-        if (value === 'v') {
-          direction = 'DOWN';
-        }
-        location = { location: [x, y], direction }
+        location = { location: [x, y], direction: DIRECTIONS[value] }
       }
     });
   });
-
   return location;
 }
 
