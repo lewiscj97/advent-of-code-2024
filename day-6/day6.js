92bdcc6be20973cce5baea3482ef03c35e89a330
lewis.jones <lewis1997@live.com>
2024-12-06 22:39:22 +0000

    feat: locate guard in matrix

--- day-6/day6.js
+++ day-6/day6.js
@@ -1,3 +1,34 @@
+// Need to find the location of the guard in the matrix
+// need to identify direction of travel
+// check what is in the cell directly in the direction of the guard
+// move if they can
+
+function locateGuard(matrix) {
+  let location = {}
+  matrix.forEach((row, x) => {
+    row.forEach((value, y) => {
+      if (value !== '.' && value !== '#') {
+        let direction;
+        if (value === '^') {
+          direction = 'UP';
+        }
+        if (value === '>') {
+          direction = 'RIGHT';
+        }
+        if (value === '<') {
+          direction = 'LEFT';
+        }
+        if (value === 'v') {
+          direction = 'DOWN';
+        }
+        location = { location: [x, y], direction }
+      }
+    });
+  });
+
+  return location;
+}
+
 function createMatrix(input) {
   const split = input.split('\n');
   const matrix = [];
@@ -9,4 +40,4 @@ function createMatrix(input) {
   return matrix;
 }
 
-module.exports = { createMatrix }
+module.exports = { createMatrix, locateGuard }
--- day-6/test.js
+++ day-6/test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const { createMatrix } = require('./day6');
+const { createMatrix, locateGuard } = require('./day6');
 
 
 describe('Day 6', () => {
@@ -31,5 +31,19 @@ describe('Day 6', () => {
   it('creates a matrix', () => {
     const output = createMatrix(input);
     expect(output).to.deep.equal(matrix);
+  });
+
+  it('can locate the guard', () => {
+    const expected = [6, 4];
+    const output = locateGuard(matrix);
+
+    expect(output.location).to.deep.eq(expected);
+  });
+
+  it('knows direction of travel', () => {
+    const expected = 'UP';
+    const output = locateGuard(matrix);
+
+    expect(output.direction).to.eq(expected);
   });
 });
