fa5c59ecff771487e17f8cd6173b506c23bd250a
lewis.jones <lewis1997@live.com>
2024-12-11 13:33:41 +0000

    test: assert test input works with creation of array method

--- day-9/test.js
+++ day-9/test.js
@@ -9,4 +9,13 @@ describe('Day 9', () => {
     const response = createFileArray(input);
     expect(response).to.deep.eq(expected);
   });
+
+  it('creates an expanded array representation of test input', () => {
+    const input = '2333133121414131402';
+    const expected = ['0', '0', '.', '.', '.', '1', '1', '1', '.', '.', '.', '2', '.', '.', '.', '3', '3', '3', '.', '4', '4', '.', '5', '5', '5', '5', '.', '6', '6', '6', '6', '.', '7', '7', '7', '.', '8', '8', '8', '8', '9', '9'];
+
+    const response = createFileArray(input);
+
+    expect(response).to.deep.eq(expected);
+  });
 })
