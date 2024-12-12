4d4047eda67f43682531b619a311262d3565eb8f
lewis.jones <lewis1997@live.com>
2024-12-12 11:02:10 +0000

    feat: find values and locations, create object

--- day-12/day12.js
+++ day-12/day12.js
@@ -3,6 +3,21 @@ function createMatrix(input) {
   return rows.map((row) => row.split(''));
 }
 
+function identifyValuesAndLocations(matrix) {
+  const valuesAndLocations = {};
+  matrix.forEach((row, x) => {
+    row.forEach((val, y) => {
+      if (valuesAndLocations?.[val] !== undefined) {
+        valuesAndLocations[val].push([x, y]);
+      } else {
+        valuesAndLocations[val] = [[x,y]];
+      }
+    });
+  });
+  return valuesAndLocations;
+}
+
 module.exports = {
   createMatrix,
+  identifyValuesAndLocations,
 }
--- day-12/test.js
+++ day-12/test.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
 const {
-  createMatrix
+  createMatrix,
+  identifyValuesAndLocations
 } = require('./day12');
 
 describe('Day 12', () => {
@@ -22,5 +23,29 @@ describe('Day 12', () => {
     const response = createMatrix(input);
 
     expect(response).to.deep.eq(matrix);
+  });
+
+  it('should identify each value and its coordinates', () => {
+    const expected = {
+      'A': [
+        [0,0], [0,1], [0,2], [0,3]
+      ],
+      'B': [
+        [1,0], [1,1], [2,0], [2,1]
+      ],
+      'C': [
+        [1,2], [2,2], [2,3], [3,3]
+      ],
+      'D': [
+        [1,3]
+      ],
+      'E': [
+        [3,0], [3,1], [3,2]
+      ],
+    };
+
+    const response = identifyValuesAndLocations(matrix);
+
+    expect(response).to.deep.eq(expected);
   });
 });
